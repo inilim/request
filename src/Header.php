0c5d62e6a06d860fde0986dc541f7b0ebdb8ae0e
Dmitry <40324321+inilim@users.noreply.github.com>
2024-02-25 00:46:20 +0300

    Update Header.php

--- src/Header.php
+++ src/Header.php
@@ -28,21 +28,24 @@ class Header
 
         if (\function_exists('getallheaders')) {
             $headers = \getallheaders();
-            if ($headers !== false) return $headers;
+            if ($headers !== false) {
+                return \array_change_key_case($headers, CASE_LOWER);
+            }
         }
 
         foreach ($_SERVER as $name => $value) {
+            /** @var string $name */
             if (
                 ($http = \str_starts_with($name, 'HTTP_'))
                 ||
                 $name == 'CONTENT_TYPE' || $name == 'CONTENT_LENGTH'
             ) {
                 if ($http) $name = \substr($name, 5);
-                $name = \strtolower(\str_replace('_', '-', $name));
+                $name = \str_replace('_', '-', $name);
                 $headers[$name] = $value;
             }
         }
 
-        return $headers;
+        return \array_change_key_case($headers, CASE_LOWER);
     }
 }
